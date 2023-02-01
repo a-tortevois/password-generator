ebc3ee500984b5f4adf02623909802f15c262402
a-tortevois <alexandre.tortevois@gmail.com>
2023-02-01 21:12:24 +0100

    fix lint errors & wanings

--- extension/popup.js
+++ extension/popup.js
@@ -2,7 +2,7 @@
 const i18n = document.querySelectorAll("[data-intl]");
 const i18nTitle = document.querySelectorAll("[data-intl-title]");
 i18n.forEach((msg) => {
-  msg.innerHTML = chrome.i18n.getMessage(msg.dataset.intl || msg.id);
+  msg.textContent = chrome.i18n.getMessage(msg.dataset.intl || msg.id);
 });
 i18nTitle.forEach((msg) => {
   msg.title = chrome.i18n.getMessage(msg.getAttribute("data-intl-title"));
